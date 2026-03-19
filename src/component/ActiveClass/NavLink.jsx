75f451ca09d65ef9853344762b3a7fa2887b11ab
nikel123-sketch <webdevelopernikel@gmail.com>
2026-03-19 21:11:27 +0800

    active class added

---
+++ src/component/ActiveClass/NavLink.jsx
@@ -0,0 +1,18 @@
+"use client"
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const NavLink = ({ href, children }) => {
+  const pathname = usePathname();
+
+  const isActive = pathname == href;
+
+  return (
+    <div>
+      <Link className={`${isActive? 'underline font-bold text-amber-500':''}`} href={href}>{children}</Link>
+    </div>
+  );
+};
+
+export default NavLink;
--- src/component/shared/Navber.jsx
+++ src/component/shared/Navber.jsx
@@ -1,16 +1,17 @@
 import Link from 'next/link';
 import React from 'react';
+import NavLink from '../ActiveClass/NavLink';
 
 const Navber = () => {
     return (
         <div className=' flex justify-between items-center  bg-violet-950 p-3'>
             <h1 className='font-bold text-3xl'>Next.js</h1>
             <div className="flex gap-3">
-                <Link href={'/'}>Home</Link>
-                <Link href={'/login'}>Login</Link>
-                <Link href={'/register'}>Register</Link>
-                <Link href={'/about'}>About</Link>
-                <Link href={'/about/tutorials'}>Tutorials</Link>
+                <NavLink href={'/'}>Home</NavLink>
+                <NavLink href={'/login'}>Login</NavLink>
+                <NavLink href={'/register'}>Register</NavLink>
+                <NavLink href={'/about'}>About</NavLink>
+                <NavLink href={'/about/tutorials'}>Tutorials</NavLink>
                
             </div>
         </div>
